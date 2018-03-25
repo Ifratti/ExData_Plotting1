21e9198e84d98a607be6f0b2eb1d986b5c3cc72f
Isaac Fratti <frattister@gmail.com>
2018-03-24 20:45:12 -0400

    Remove file location

--- PlotAll.R
+++ PlotAll.R
@@ -1,4 +1,4 @@
-Directory<-"C:/Users/ifrat/Dropbox/R/Coursera/Exploratory data Analysis/ExData_Plotting1/"
+
 for(i in 1:4){
-    source(paste0(Directory,'Plot',i,'.R'))
+    source(paste0('Plot',i,'.R'))
 }
